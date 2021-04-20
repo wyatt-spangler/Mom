9b07efcadb2e38197fa7ede251a0cc585a0c0d78
Wyatt Spangler <wyatt.spa@outlook.com>
2021-04-20 11:09:57 -0400

    Add project files.

---
+++ Demo/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public Author Author { get; set; }
+    }
+}
---
+++ Demo/Query.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Query
+    {
+        public Book GetBook() =>
+            new Book
+            {
+                Title = "C# in depth.",
+                Author = new Author
+                {
+                    Name = "Jon Skeet"
+                }
+            };
+    }
+}
